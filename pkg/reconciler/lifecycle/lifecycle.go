ad60e37ecc1483fae10158301e60d9ba396f9077
Yael Harel <yharel@vmware.com>
2022-11-14 16:43:06 -0500

    WIP - Tom
    Signed-off-by: Yael Harel <yharel@vmware.com>

--- pkg/reconciler/lifecycle/lifecycle.go
+++ pkg/reconciler/lifecycle/lifecycle.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/pivotal/kpack/pkg/config"
+	"github.com/pivotal/kpack/pkg/tracker"
 
 	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
@@ -22,32 +23,40 @@ import (
 
 func NewController(
 	ctx context.Context,
+	opt reconciler.Options,
+	k8sClient k8sclient.Interface,
 	configmapName string,
 	configMapInformer coreinformers.ConfigMapInformer,
+	lifecycleProvider *config.LifecycleProvider,
 ) *controller.Impl {
 	key := types.NamespacedName{
 		Namespace: system.Namespace(),
 		Name:      configmapName,
 	}
 
-	wh := &Reconciler{
-		key: key,
+	c := &Reconciler{
+		Key:               key,
+		ConfigMapLister:   configMapInformer.Lister(),
+		K8sClient:         k8sClient,
+		LifecycleProvider: lifecycleProvider,
 	}
 
 	const queueName = "lifecycle"
-	c := controller.NewContext(ctx, wh, controller.ControllerOptions{WorkQueueName: queueName, Logger: logging.FromContext(ctx).Named(queueName)})
+	impl := controller.NewContext(ctx, c, controller.ControllerOptions{WorkQueueName: queueName, Logger: logging.FromContext(ctx).Named(queueName)})
 
 	// Reconcile when the lifecycle configmap changes.
 	configMapInformer.Informer().AddEventHandler(cache.FilteringResourceEventHandler{
 		FilterFunc: controller.FilterWithNameAndNamespace(key.Namespace, key.Name),
-		Handler:    controller.HandleAll(c.Enqueue),
+		Handler:    controller.HandleAll(impl.Enqueue),
 	})
 
-	return c
+	c.Tracker = tracker.New(impl.EnqueueKey, opt.TrackerResyncPeriod())
+
+	return impl
 }
 
 type Reconciler struct {
-	key               types.NamespacedName
+	Key               types.NamespacedName
 	ConfigMapLister   corelisters.ConfigMapLister
 	Tracker           reconciler.Tracker
 	K8sClient         k8sclient.Interface
@@ -87,6 +96,9 @@ func (c *Reconciler) reconcileLifecycleImage(ctx context.Context, configMap *cor
 		Data:
 		  image: "registry.io/foo"
 	*/
+
+	// TODO - ask whether we really need to check the digest or we can just call UpdateImage
+	// do we care if the image ref changes as long as the digest is the same (i.e same image in gcr vs ecr)
 	imageRef, ok := configMap.Data[config.LifecycleConfigKey]
 	if !ok {
 		return errors.Errorf("%s config invalid", config.LifecycleConfigName)
@@ -99,7 +111,8 @@ func (c *Reconciler) reconcileLifecycleImage(ctx context.Context, configMap *cor
 	if ref.Identifier() == digest {
 		return nil
 	}
-	c.LifecycleProvider.UpdateImage()
+
+	c.LifecycleProvider.UpdateImage(configMap)
 	/*
 		imageRef == "registry.io/foo
 		OR
